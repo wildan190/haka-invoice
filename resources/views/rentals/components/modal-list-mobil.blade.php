56d1c2f8ac6cb437c801041691b72375961b44cd
Wildan Belfiore <mikewildan@outlook.com>
2025-03-06 20:34:22 +0700

    new file:   resources/views/rentals/components/modal-list-mobil.blade.php 	modified:   resources/views/rentals/create.blade.php

---
+++ resources/views/rentals/components/modal-list-mobil.blade.php
@@ -0,0 +1,43 @@
+<!-- Modal untuk memilih mobil -->
+<div class="modal fade" id="modalListMobil" tabindex="-1" aria-labelledby="modalListMobilLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalListMobilLabel">Pilih Mobil</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Merk</th>
+                                <th>Type</th>
+                                <th>Harga</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($mobils as $mobil)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $mobil->merk }}</td>
+                                    <td>{{ $mobil->type }}</td>
+                                    <td>{{ number_format($mobil->price) }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary btn-pilih-mobil"
+                                            data-id="{{ $mobil->id }}" data-merk="{{ $mobil->merk }}"
+                                            data-type="{{ $mobil->type }}" data-bs-dismiss="modal">
+                                            Pilih
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
